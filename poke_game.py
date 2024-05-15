e06585c5f20bfaf1603adcd9385a4d45ec53d2dd
Rajpal Aujla <raujla@spartaglobal.com>
2024-05-15 16:30:48 +0100

    added stat breakdown to Luis code

--- poke_game.py
+++ poke_game.py
@@ -1,15 +1,24 @@
-import requests
 import json
+import requests
+
+import requests
 
 url = 'https://pokeapi.co/api/v2/pokemon/?limit=150&offset=0'
 response = requests.get(url)
-pokemon_list = json.loads(response.text)['results']
+pokemon_list = response.json()['results']
 
-pokemon = input('give me a pokemon')
+pokemon_name = input('Give me a Pokémon: ')
 
-for n in pokemon_list:
-    if n['name'] == pokemon:
-        response = requests.get(n['url'])
-        print(response.json())
+for pokemon in pokemon_list:
+    if pokemon['name'] == pokemon_name.lower():
+        pokemon_url = pokemon['url']
+        pokemon_data = requests.get(pokemon_url).json()
+        print("Name:", pokemon_name.capitalize())
+        print("Stats:")
+        for stat in pokemon_data['stats']:
+            print(f"{stat['stat']['name'].capitalize()}: {stat['base_stat']}")
+        break
+else:
+    print("Pokémon not found:", pokemon_name.capitalize())
 
 
